58eb24b2ad8f2a95760cf4d987e3eb6601fe22b4
zsoltibv <zsoltibv@gmail.com>
2023-10-08 11:45:20 +0300

    EXP-60 Added default currencies and categories data insertion to db

---
+++ src/main/resources/database/data.sql
@@ -0,0 +1,35 @@
+-- Insert RON
+INSERT IGNORE  INTO `currency` (`code`)
+VALUES ('RON');
+
+-- Insert USD
+INSERT IGNORE INTO `currency` (`code`)
+VALUES ('USD');
+
+-- Insert EUR
+INSERT IGNORE INTO `currency` (`code`)
+VALUES ('EUR');
+
+-- Insert "Food & Restaurants"
+INSERT IGNORE INTO `category` (`description`, `color`)
+VALUES ('Food & Restaurants', '#5338F5');
+
+-- Insert "Car"
+INSERT IGNORE INTO `category` (`description`, `color`)
+VALUES ('Car', '#1EB3DB');
+
+-- Insert "Entertainment"
+INSERT IGNORE INTO `category` (`description`, `color`)
+VALUES ('Entertainment', '#FF8084');
+
+-- Insert "Subscriptions"
+INSERT IGNORE INTO `category` (`description`, `color`)
+VALUES ('Subscriptions', '#E83FAB');
+
+-- Insert "Coffee"
+INSERT IGNORE INTO `category` (`description`, `color`)
+VALUES ('Coffee', '#76A305');
+
+-- Insert "House Hold"
+INSERT IGNORE INTO `category` (`description`, `color`)
+VALUES ('House Hold', '#17C99E');
