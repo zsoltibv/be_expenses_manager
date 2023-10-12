9454760cef38ace2eb641ce195e1506d9d70cec6
Stefan <stefanfrusi@gmail.com>
2023-10-11 23:10:14 -0700

    changed to path variable

--- src/main/java/com/endava/expensesmanager/controller/ExpenseController.java
+++ src/main/java/com/endava/expensesmanager/controller/ExpenseController.java
@@ -33,8 +33,8 @@ public class ExpenseController {
         List<ExpenseDto> expenses = expenseService.getExpensesByUserId(userId, startDate, endDate);
         return new ResponseEntity<>(expenses, HttpStatus.OK);
     }
-    @DeleteMapping("/delete")
-    public ResponseEntity<?> deleteExpenseById(@RequestParam Integer expenseId)
+    @DeleteMapping("/expense/{expenseId}")
+    public ResponseEntity<?> deleteExpenseById(@PathVariable Integer expenseId)
     {
         return expenseService.deleteExpenseById(expenseId);
     }
